079116d412485bb42ccea2ab93b29d0efe32e84f
Arseniy Sviridov <92201498+RendSv@users.noreply.github.com>
2023-03-11 11:22:40 +0500

    Add files via upload

---
+++ rashireniya/content-script.js
@@ -0,0 +1,12 @@
+function deleteBlock() {
+    !!document
+    .getElementsByClassName(
+        "ContributionCalendar-day"
+        ) &&
+        document
+    .getElementsByClassName(
+        "ContributionCalendar-day"
+        )[0].remove();
+}
+
+deleteBlock();
